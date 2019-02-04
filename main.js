3bf162ef65f6e7edb4a10613d0e0439263e5fefc
1-nty <46821853+1-nty@users.noreply.github.com>
2019-02-04 18:12:34 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,22 @@
+var myObjFunction = {
+    oncall: function(){
+    console.log("Youve called a function!")
+  }
+  
+}
+
+var var1 = new myObjFunction.oncall
+
+
+/****************************************************************************************/
+
+
+
+var f = {
+    oncall: function(){
+    console.log("Youve called a function!")
+  }
+  
+}
+
+var var1 = new f.oncall
